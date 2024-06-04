dd14e5fdac4603a5fd08b870c692b28c724774b9
sahebrao jadhav <onlysahebpatil@gmail.com>
2024-06-04 07:34:41 +0530

    error occured

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
+import React from 'react'
+import Navbar from './components/navbar/Navbar';
+//import Navbar from './components/navbar/Navbar.jsx'
 
-import './App.css'
+console.log("app.jsx is working");
 
 function App() {
-  
-
   return (
-    <>
-      Movie Application
-
-    </>
+    <div>
+      sahebrao jadhav from this side 
+      hey guys
+      <Navbar/>
+    </div>
   )
 }
 
